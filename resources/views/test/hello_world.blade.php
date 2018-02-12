4d71fc78834e88a3559d41a218d2fff50d43c160
Core <hfreedomx@gmail.com>
2018-02-13 02:00:22 +0900

    [  1. Adding header and footer blade templates  2. Using @include function to include header and footer templates and reuse them ]

--- resources/views/test/hello_world.blade.php
+++ resources/views/test/hello_world.blade.php
@@ -1,10 +1,12 @@
-<html>
-    <body>
-        <!-- you can use trans to load language setting, the argument is [language file name . key name] -->
-        <!-- you can use dot to connect the output from two functions -->
-        <h1>{{ trans('hello_world.HELLO').trans('hello_world.WORLD')}} !!!!!</h1>
-        <h2>views/test/hello_world.blade.php</h2>
-        <!-- it will just return the argument as text when there is no key in both of local and default language templates -->
-        <h3>{{ trans('hello_world.NoKey') }}</h3>
-    </body>
-<html>
+@include('common.header')
+{{-- you can use @include function for including an existing blade template  --}}
+<!-- the above method for comment will not display in output HTML file -->
+
+<!-- you can use trans to load language setting, the argument is [language file name . key name] -->
+<!-- you can use dot to connect the output from two functions -->
+<h1>{{ trans('hello_world.HELLO').trans('hello_world.WORLD')}} !!!!!</h1>
+<h2>views/test/hello_world.blade.php</h2>
+<!-- it will just return the argument as text when there is no key in both of local and default language templates -->
+<h3>{{ trans('hello_world.NoKey') }}</h3>
+
+@include('common.footer')
